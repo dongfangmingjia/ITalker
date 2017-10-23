7f23a7d32b01050dd385646c550d937931052617
wangdongjian <wangdongjian@suiyueyule.com>
2017-10-23 18:28:50 +0800

    修改main activity

--- app/src/main/java/com/warner/italker/MainActivity.java
+++ app/src/main/java/com/warner/italker/MainActivity.java
@@ -1,13 +1,27 @@
 package com.warner.italker;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.warner.common.app.app.Activity;
+
+import butterknife.BindView;
+
+/**
+ * @author warner
+ */
+public class MainActivity extends Activity {
+
+	@BindView(R.id.tv_text)
+	TextView tv_text;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	protected int getcontentLayoutId() {
+		return R.layout.activity_main;
+	}
+
+	@Override
+	protected void initWidget() {
+		super.initWidget();
+		tv_text.setText("Hello 你好");
 	}
 }
